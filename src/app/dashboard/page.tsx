e84d891891e868113223f7328be2739ba5c550e8
emilitogaza <144819695+emilitogaza@users.noreply.github.com>
2025-08-19 09:56:03 +0200

    Updated default time frame
    Now it defaults to all time, instead of last 30 days. That way we initially see a better overview and can narrow it down if we want to.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -37,7 +37,7 @@ export default async function Dashboard() {
           </div>
         </div>
       </div>
-      <TimeFrameProvider initialTimeFrame="last30days">
+      <TimeFrameProvider initialTimeFrame="all">
         <DashboardContent
           session={session}
           positiveMessage={positiveMessage}
